2c672d1e631dd23ef290b01b7fb5dddd3bcb2f64
sanjeevpuspam <sanjeev.puspam@gmail.com>
2022-02-01 17:06:33 +0530

    modify js

--- js/index.js
+++ js/index.js
@@ -212,4 +212,9 @@ $('.to-top').click(function(){
           console.log('Oops... ' + JSON.stringify(error));
       });
   });
+  $("a.iframe").fancybox({
+    'width': 640, // or whatever you want
+    'height': 480, // or whatever you want
+    'type': 'iframe'
+  });
 })
